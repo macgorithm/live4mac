0ee0f876c32f45255ab1c5cb05143841af571ea0
hean <hean1103@naver.com>
2022-04-25 19:45:26 +0900

    Fix : Solve boj 17178 줄서기 (혜안)

--- 2022/Apr/week_4/boj_17178_줄서기_경혜안.java
+++ 2022/Apr/week_4/boj_17178_줄서기_경혜안.java
@@ -2,6 +2,7 @@ package May;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Stack;
 import java.util.StringTokenizer;
@@ -15,7 +16,20 @@ public class boj_17178_줄서기_경혜안 {
 		
 		String[][] line = new String[n][5];
 		
-		PriorityQueue<String> order = new PriorityQueue<String>();
+		PriorityQueue<String> order = new PriorityQueue<String>(new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				String[] t1 = o1.split("-");
+				String[] t2 = o2.split("-");
+				
+				if(t1[0].equals(t2[0])) { // 알파벳이 같다면 
+					return Integer.parseInt(t1[1]) - Integer.parseInt(t2[1]);
+				} else {
+					return t1[0].compareTo(t2[0]);
+				}
+			}
+		});
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(bf.readLine());
